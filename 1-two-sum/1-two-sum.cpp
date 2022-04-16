2113197be2ec65f8be8415c8f725737ebfe5d310
Syed Arsal Abbas <83390475+ArsalAbbas@users.noreply.github.com>
2022-04-17 02:05:14 +0530

    Time: 22 ms (53.16%), Space: 12.1 MB (7.43%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -3,20 +3,36 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-   vector<int> res;
-        for(auto i=nums.begin(); i<nums.end()-1;i++){
-            for(auto j=i+1;j<nums.end(); j++){
+//    vector<int> res;
+//         for(auto i=nums.begin(); i<nums.end()-1;i++){
+//             for(auto j=i+1;j<nums.end(); j++){
                 
-                int sum=nums.at(i-nums.begin())+nums.at(j-nums.begin());//line 11
-                if(sum==target){
-                    res.push_back(i-nums.begin());
-                    res.push_back(j-nums.begin());
-                    return res;
-                    break;
-                }
+//                 int sum=nums.at(i-nums.begin())+nums.at(j-nums.begin());//line 11
+//                 if(sum==target){
+//                     res.push_back(i-nums.begin());
+//                     res.push_back(j-nums.begin());
+//                     return res;
+//                     break;
+//                 }
+//             }
+//         }
+//         res.clear();
+//         return res;
+        vector<int> ans;
+        unordered_map<int,int> mapping;
+        
+        for(int i=0;i<nums.size();i++){
+            
+            if(mapping.find(target-nums[i])!=mapping.end()){
+                
+                ans.push_back(mapping[target-nums[i]]);
+                ans.push_back(i);
+                
             }
-        }
-        res.clear();
-        return res;
+            
+            mapping[nums[i]]=i;
+            
+        }return ans;
+        
     }
 };
