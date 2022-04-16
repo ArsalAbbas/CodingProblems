b1da0c40f98fb025f3db054f7ba4398189f675d7
Syed Arsal Abbas <83390475+ArsalAbbas@users.noreply.github.com>
2022-04-17 01:46:28 +0530

    Time: 31 ms (32.98%), Space: 19.6 MB (40.00%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,7 +1,41 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        return nums[nums.size()/2];    
+        // Sol 1- nlogn
+        // sort(nums.begin(),nums.end());
+        // return nums[nums.size()/2];    
+            // cout<<*max_element(nums.begin(),nums.end())<<endl;
+        
+        
+        //Sol 2 - memory limit
+        //     vector<int> mapping(INT_MAX,0);
+        //     for(int i=0; i< nums.size(); i++)
+        //     {
+        //         mapping[nums[i]]++;
+        //     }
+        // int maxVote=0;
+        // int maxElem;
+        // for(int i=0; i< nums.size(); i++)
+        //     {
+        //     if(mapping[nums[i]]>maxVote)
+        //         maxElem=nums[i];
+        //     }
+        // return maxElem;
+        
+        //Sol 3 - Moore's voting algorithm
+        
+        int candidate=nums[0],freq=1;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]==candidate)
+                freq++;
+            if(nums[i]!= candidate)
+                freq--;
+            if(freq==0)
+            {
+                candidate=nums[i];
+                freq=1;
+            }
+        }
+        return candidate;
     }
 };
