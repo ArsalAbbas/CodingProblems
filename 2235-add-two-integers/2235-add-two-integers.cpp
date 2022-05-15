ec729e1dbbca8b7f3e51badea07e13f35144610e
Syed Arsal Abbas <83390475+ArsalAbbas@users.noreply.github.com>
2022-05-15 09:47:52 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (74.80%) - LeetHub

--- 2235-add-two-integers/2235-add-two-integers.cpp
+++ 2235-add-two-integers/2235-add-two-integers.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
-    int sum(int num1, int num2) {
-        return num1+num2;
-    }
+    int sum(int x,int y){
+	if(y==0)
+        return x;
+	else if(x<0 && y<0){
+		int add = ~x^~y;
+		int carry = (~x&~y)<<1;
+		return ~sum(sum(add,carry),1);
+	}
+	else{
+		int add = x^y;
+		int carry = (x&y)<<1;
+		return sum(add,carry);
+	}
+
+}
 };
