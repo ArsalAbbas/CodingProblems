cc1d6b346dccbf9b89c0127c07a64154bc6f7fe3
Syed Arsal Abbas <83390475+ArsalAbbas@users.noreply.github.com>
2022-06-06 12:48:11 +0530

    Time: 6 ms (6.76%), Space: 6 MB (69.77%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -1,18 +1,30 @@
-class Solution {
+// class Solution {
+// public:
+//     string defangIPaddr(string address) {
+//         string str="";
+//         for(int i=0; i< address.length() ; i++){
+            
+//             if(address[i]=='.')
+//                 str=str+"[.]";
+//             else
+//                 str=str+address[i];
+            
+            
+//         }
+        
+//         return str;
+        
+//     }
+// };
+class Solution 
+{
 public:
-    string defangIPaddr(string address) {
-        string str="";
-        for(int i=0; i< address.length() ; i++){
-            
-            if(address[i]=='.')
-                str=str+"[.]";
-            else
-                str=str+address[i];
-            
-            
+    string defangIPaddr(string ad) 
+    {
+        for(int i=0;i<ad.size();i++)
+        {
+            if(ad[i]=='.'){ad.replace(i,1,"[.]");i+=2;}  
         }
-        
-        return str;
-        
+        return ad;
     }
 };
