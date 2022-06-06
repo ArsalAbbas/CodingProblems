8b5157930c782239f5c2169b77a972c514ad96f9
Syed Arsal Abbas <83390475+ArsalAbbas@users.noreply.github.com>
2022-06-06 12:52:16 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (22.73%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -2,29 +2,7 @@ class Solution {
 public:
     string defangIPaddr(string address) {
         string str="";
-        for(int i=0; i< address.length() ; i++){
-            
-            if(address[i]=='.')
-                str=str+"[.]";
-            else
-                str=str+address[i];
-            
-            
-        }
-        
+        for(int i=0; i< address.length() ; i++)
+            str=address[i]=='.'?str+"[.]":str+address[i];       
         return str;
-        
-    }
-};
-// class Solution 
-// {
-// public:
-//     string defangIPaddr(string ad) 
-//     {
-//         for(int i=0;i<ad.size();i++)
-//         {
-//             if(ad[i]=='.'){ad.replace(i,1,"[.]");i+=2;}  
-//         }
-//         return ad;
-//     }
-// };
+}};
