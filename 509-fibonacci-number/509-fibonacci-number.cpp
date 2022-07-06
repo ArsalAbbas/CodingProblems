3ccc1180f39b6a818889f1d49b9cfa78066d4ba2
Syed Arsal Abbas <83390475+ArsalAbbas@users.noreply.github.com>
2022-07-06 09:36:14 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (41.61%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int fib(int n) {
-        // if(n==0)
-        //     return 0;
-        // if(n==1)
-        //     return 1;
-        // // return fib(n-1)+fib(n-2);
-        return (n==0) ? 0 : (n==1) ? 1 : fib(n-1) + fib(n-2);
-        // int x=0;
-        // int y=1;
-        // int temp;
-        // for(int i=0; i<n ; i++){
-        //     temp=y;
-        //     y=x;
-        //     x=x+temp;
-        // }
-        // return x;
+        if(n==0)
+            return 0;
+        if(n==1)
+            return 1;
+        // return fib(n-1)+fib(n-2);
+        int x=0;
+        int y=1;
+        int temp;
+        for(int i=0; i<n ; i++){
+            temp=y;
+            y=x;
+            x=x+temp;
+        }
+        return x;
             
     }
 };
