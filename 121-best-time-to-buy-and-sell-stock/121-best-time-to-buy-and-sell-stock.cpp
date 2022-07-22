7197273edc1fe96e0974bddae43196c1a92f5efc
Syed Arsal Abbas <83390475+ArsalAbbas@users.noreply.github.com>
2022-07-23 00:03:14 +0530

    Time: 221 ms (35.77%), Space: 93.4 MB (11.99%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,25 +1,25 @@
-// class Solution {
-// public:
-//     int maxProfit(vector<int>& prices) {
-//         int price=INT_MAX;
-//         int profit=0;
-//     for(int i=0; i< prices.size() ; i++){
-        
-//         price=min(price,prices[i]);
-//         profit=max(profit, prices[i]-price);
-        
-//     }
-//         return price;
-//     }
-// };
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int ans = 0, minBuy = INT_MAX;
-        for(int &x:prices){
-            minBuy = min(minBuy,x);
-            ans = max(ans,x-minBuy);
-        }
-        return ans;
+        int price=INT_MAX;
+        int profit=0;
+    for(int i=0; i< prices.size() ; i++){
+        
+        price=min(price,prices[i]);
+        profit=max(profit, prices[i]-price);
+        
+    }
+        return profit;
     }
 };
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int ans = 0, minBuy = INT_MAX;
+//         for(int &x:prices){
+//             minBuy = min(minBuy,x);
+//             ans = max(ans,x-minBuy);
+//         }
+//         return ans;
+//     }
+// };
