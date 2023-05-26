33fed031d47f02f578ada199b03a52e55ba1c745
Syed Arsal Abbas <83390475+ArsalAbbas@users.noreply.github.com>
2023-05-27 01:58:53 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -6,32 +6,65 @@ using namespace std;
 class Solution
 {
     private:
-    void dfs(vector<int> &vis,vector<int> adj[], stack<int> &st, int cur){
-        vis[cur]=1;
-        for(auto it: adj[cur])
-            if(!vis[it]){
-            dfs(vis,adj,st,it);
-            }
-            st.push(cur);    
-    }
+    // void dfs(vector<int> &vis,vector<int> adj[], stack<int> &st, int cur){
+    //     vis[cur]=1;
+    //     for(auto it: adj[cur])
+    //         if(!vis[it]){
+    //         dfs(vis,adj,st,it);
+    //         }
+    //         st.push(cur);    
+    // }
+    //   void bfs(vector<int> &vis,vector<int> adj[], queue<int> &q,vector<int> &ans,vector<int> &indegree){
+    //         while(!q.empty()){
+    //               int cur=q.front();
+    //               q.pop();
+    //               ans.push_back(cur);
+    //               vis[cur]=1;
+    //               for(auto it: adj[cur]){
+    //                   indegree[it]--;
+    //                   if(indegree[it]==0 &&!vis[it]) 
+    //                     q.push(it);
+    //               }
+    //         }
+    // }
 	public:
 	//Function to return list containing vertices in Topological order. 
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
-	    vector<int> vis(V,0);
-	    stack<int> st;
+	   // vector<int> vis(V,0);
+	   // stack<int> st;
 	    
-	    for(int i=0; i<V; i++)
-	        if(!vis[i]) dfs(vis,adj,st,i);
+	   // for(int i=0; i<V; i++)
+	   //     if(!vis[i]) dfs(vis,adj,st,i);
 	       
-	   vector<int> res; 
-	   while(!st.empty()){
-	       res.push_back(st.top());
-	       st.pop();
+	   //vector<int> res; 
+	   //while(!st.empty()){
+	   //    res.push_back(st.top());
+	   //    st.pop();
+	   //}
+	   vector<int> indegree(V,0);
+	   for(int i=0; i<V; i++){
+	       for(auto it: adj[i])
+	            indegree[it]++;
 	   }
-	   
+	   vector<int> res;
+	   queue<int> q;
+	   for(int i=0; i<V; i++){
+	       if(!indegree[i]) q.push(i);
+	   }
+	   while(!q.empty()){
+                  int cur=q.front();
+                  q.pop();
+                  res.push_back(cur);
+
+                  for(auto it: adj[cur]){
+                      indegree[it]--;
+                      if(indegree[it]==0) 
+                        q.push(it);
+                  }
+            }
+	   //for(auto it: res) cout<<it<<endl;
 	   return res;
-	    
 	}
 };
 
